ba8cbdac4212434effa626b91dc2debb56ea54a4
davide16 <davide.sciammetta@gmail.com>
2020-01-08 17:10:56 +0100

    Create InvoiceType.php

---
+++ src/AppBundle/Form/InvoiceType.php
@@ -0,0 +1,30 @@
+<?php
+// src/AppBundle/Form/InvoiceType.php
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Invoice;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InvoiceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            
+            ->add('invoice_date', DateType::class)
+            ->add('invoice_num', IntegerType::class)
+            ->add('id_client', IntegerType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Invoice::class,
+        ]);
+    }
+}
